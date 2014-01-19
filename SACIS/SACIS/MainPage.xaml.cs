8b003f6f6011773c4901408588df5b93ee45fc8f
burnermanx <burnermanx@gmail.com>
2014-01-18 22:48:14 -0200

    First commit

---
+++ SACIS/SACIS/MainPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using SACIS.Resources;
+
+namespace SACIS
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Botao_Entrar(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Principal.xaml", UriKind.Relative));
+        }
+
+        private void AppBar_Entrar(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Principal.xaml", UriKind.Relative));
+        }
+
+        private void Menu_Sobre(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Sobre.xaml", UriKind.Relative));
+        }
+
+        private void LoginLostFocus(object sender, RoutedEventArgs e)
+        {
+            CheckLoginWatermark();
+        }
+
+        public void CheckLoginWatermark()
+        {
+            var loginEmpty = string.IsNullOrEmpty(Login.Text);
+            LoginWatermark.Opacity = loginEmpty ? 100 : 0;
+            Login.Opacity = loginEmpty ? 0 : 100;
+        }
+
+        private void LoginGotFocus(object sender, RoutedEventArgs e)
+        {
+            LoginWatermark.Opacity = 0;
+            Login.Opacity = 100;
+        }
+
+        private void PasswordLostFocus(object sender, RoutedEventArgs e)
+        {
+            CheckPasswordWatermark();
+        }
+
+        public void CheckPasswordWatermark()
+        {
+            var passwordEmpty = string.IsNullOrEmpty(Password.Password);
+            PasswordWatermark.Opacity = passwordEmpty ? 100 : 0;
+            Password.Opacity = passwordEmpty ? 0 : 100;
+        }
+
+        private void PasswordGotFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordWatermark.Opacity = 0;
+            Password.Opacity = 100;
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
---
+++ SACIS/SACIS/Principal.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace SACIS
+{
+    public partial class PivotPage1 : PhoneApplicationPage
+    {
+        public PivotPage1()
+        {
+            InitializeComponent();
+            ApplicationBar = ((ApplicationBar)Application.Current.Resources["MsgBar"]);
+            
+            /*
+            ApplicationBar = new ApplicationBar();
+
+            ApplicationBar.Mode = ApplicationBarMode.Default;
+            ApplicationBar.Opacity = 1.0;
+            ApplicationBar.IsVisible = true;
+            ApplicationBar.IsMenuEnabled = true; */
+        }
+
+        private void Pivot_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Pivot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (((Pivot)sender).SelectedIndex)
+            {
+                case 0:
+                    ApplicationBar = ((ApplicationBar)Application.Current.Resources["MsgBar"]);
+                    break;
+
+                case 1:
+                    ApplicationBar = ((ApplicationBar)Application.Current.Resources["StorBar"]);
+                    break;
+            }
+        }
+    }
+}
